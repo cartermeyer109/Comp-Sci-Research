195943abe0c7031a4c8343a42c6fbe0697fc8a3e
Carter Meyer <67216868+cartermeyer109@users.noreply.github.com>
2024-04-23 17:39:19 -0500

    SIM plus water sensor code

---
+++ SIMCard/SIMCard.ino
@@ -0,0 +1,16 @@
+//GSM gsmAccess - base class for all GSM functions.
+//GSM_SMS sms - base class for all GSM functions for SMS.
+//gsmAccess.begin(pin) - connects to the GSM network with the pin number as a parameter, e.g. 0123.
+//sms.beginSMS(number); - creates an SMS for a specific number.
+//sms.print(message); - prints the content of the SMS.
+//sms.endSMS() - sends the SMS.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
--- WaterSensor/WaterSensor.ino
+++ WaterSensor/WaterSensor.ino
@@ -1,9 +1,27 @@
+const int data = A0;
+int value = 0;
+
+const int PUMP_PIN = 13; // Water Pump Pin
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(PUMP_PIN, OUTPUT);
+  digitalWrite(PUMP_PIN, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  value = analogRead(data);
+  Serial.println(value);
 
+  if (value > 600) {
+    digitalWrite(PUMP_PIN, HIGH);
+  }
+  else {
+    digitalWrite(PUMP_PIN, LOW);
+  }
+  delay(1000);
 }
+
+//600 is halfway point
